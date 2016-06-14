685589b4ebebc3f2cc2755bee368bab80937baff
carloserodriguez2000 <carloserodriguez2000@yahoo.com>
2016-06-14 15:50:11 -0500

    Initial verison of Weather report using ENUM for DAYS and FORCAST. Generates a random forcast for sevendays and then prints the results on the Console

---
+++ EnumPracticeWeather/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPracticeWeather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loopMore;
+            Weather myWeather = new Weather();
+
+            do
+            {
+                myWeather.GetWeatherWeek();
+                Console.WriteLine("Weather for {0} IS {1}", myWeather.GetWeekName(Weather.Days.Mon), myWeather.GetForecast(Weather.Days.Mon));
+                Console.WriteLine("Weather for {0} IS {1}", Enum.GetName(typeof(Weather.Days), Weather.Days.tue), myWeather.GetForecast(Weather.Days.tue));
+                Console.WriteLine("Weather for {0} IS {1}", Enum.GetName(typeof(Weather.Days), Weather.Days.Wed), myWeather.GetForecast(Weather.Days.Wed));
+                Console.WriteLine("Weather for {0} IS {1}", Enum.GetName(typeof(Weather.Days), Weather.Days.thu), myWeather.GetForecast(Weather.Days.thu));
+                Console.WriteLine("Weather for {0} IS {1}", Enum.GetName(typeof(Weather.Days), Weather.Days.Fri), myWeather.GetForecast(Weather.Days.Fri));
+                Console.WriteLine("Weather for {0} IS {1}", Enum.GetName(typeof(Weather.Days), Weather.Days.Sat), myWeather.GetForecast(Weather.Days.Sat));
+                Console.WriteLine("Weather for {0} IS {1}", Enum.GetName(typeof(Weather.Days), Weather.Days.Sun), myWeather.GetForecast(Weather.Days.Sun));
+                Console.WriteLine("Press Y to re-print forcast:");
+                char keyIn = Console.ReadKey().KeyChar;
+                if (keyIn == 'y' || keyIn == 'Y')
+                    loopMore = true;
+                else
+                    loopMore = false;
+            }
+            while (loopMore == true);
+        }// END OF MAIN
+    } // END OF cLASS
+} //END OF NAMESPACE
---
+++ EnumPracticeWeather/Weather.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPracticeWeather
+{
+    class Weather
+    {
+        public enum Days { Sun, Mon, tue, Wed, thu, Fri, Sat };
+        public enum Forecast { Sunny, Overcast, Rain };
+        private Random rnd1 = new Random();
+        private Forecast[] weekForcast = new Forecast[7];
+
+        public void GetWeatherWeek()
+        {
+            Array forcastNums = Enum.GetValues(typeof(Forecast));
+            int rndDayForcastIndex = rnd1.Next(forcastNums.Length);
+            Array weekNums = Enum.GetValues(typeof(Days));
+
+            for (int i = 0; i < weekNums.Length; i++)
+            {
+                rndDayForcastIndex = rnd1.Next(forcastNums.Length);
+                weekForcast[i] = (Forecast)forcastNums.GetValue(rndDayForcastIndex);
+            }
+        }
+        public Forecast GetForecast(Days thisday)  // change this touse the ENUM "Days"
+        {
+            Forecast dayWeather = weekForcast[(int)thisday];
+            return dayWeather;
+        }
+
+        public string GetWeekName(Days thisday)
+        {
+            return Enum.GetName(typeof(Weather.Days), thisday);
+        }
+    } //END OF CLASS
+
+}
